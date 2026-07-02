7e0d983749ab73834c5f22fc56b571ca2003b3b3
wade <amouramoramorette@gmail.com>
2026-07-02 23:34:37 +0200

    Update dm.js

--- src/commands/Moderation/dm.js
+++ src/commands/Moderation/dm.js
@@ -11,7 +11,7 @@ export default {
         .setDescription("Send a direct message to a user (Staff only)")
         .addUserOption(option =>
             option
-                .setName("users")
+                .setName("users""roles")
                 .setDescription("The user to send a DM to")
                 .setRequired(true)
         )
